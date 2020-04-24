cc7f3404774c28d02f7c64ef665999ce13578a23
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-24 11:33:29 -0400

    register and login works

--- database/users-model.js
+++ database/users-model.js
@@ -2,8 +2,13 @@ const db = require("../database/dbConfig");
 
 module.exports = {
   adduser,
+  findUser,
 };
 
 function adduser(newUser) {
   return db("users").insert(newUser, "id");
+}
+
+function findUser(something) {
+  return db("users").where(something);
 }
